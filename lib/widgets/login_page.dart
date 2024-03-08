ae41c9771866f3181a88d98dea469d563e990d3e
$Marl$ <marlonrope@hotmail.com>
2024-03-07 21:38:37 -0400

    feat: :rocket: Tela de login MKM Solutions

---
+++ lib/widgets/login_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset("assets/images/logo.png", width: 128),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(27),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 1, 14, 201),
+              Color.fromARGB(255, 53, 0, 121),
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 30),
+            const Text(
+              "Digite os dados de acesso nos campos abaixo.",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const CupertinoTextField(
+              cursorColor: Colors.pinkAccent,
+              padding: EdgeInsets.all(15),
+              placeholder: "Digite o seu e-mail",
+              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(7),
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            const CupertinoTextField(
+              padding: EdgeInsets.all(15),
+              cursorColor: Colors.pinkAccent,
+              placeholder: "Digite sua senha",
+              obscureText: true,
+              placeholderStyle: TextStyle(color: Colors.white70, fontSize: 14),
+              style: TextStyle(color: Colors.white, fontSize: 14),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(7),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            SizedBox(
+              width: double.infinity,
+              child: CupertinoButton(
+                padding: const EdgeInsets.all(17),
+                color: Colors.greenAccent,
+                child: const Text(
+                  "Acessar",
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            const SizedBox(height: 7),
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white70, width: 0.8),
+                borderRadius: BorderRadius.circular(7),
+              ),
+              child: CupertinoButton(
+                child: const Text(
+                  "Crie sua conta",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
